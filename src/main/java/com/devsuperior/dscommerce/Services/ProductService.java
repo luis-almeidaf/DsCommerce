e144f68281eea950077e117b92038faf5c16de4f
Luis Fernando <luis.almeida.faria@gmail.com>
2024-12-14 20:36:39 -0300

    Insert de produtos

--- src/main/java/com/devsuperior/dscommerce/Services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/Services/ProductService.java
@@ -1,10 +1,11 @@
 package com.devsuperior.dscommerce.Services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.devsuperior.dscommerce.dtos.ProductDto;
 import com.devsuperior.dscommerce.entities.Product;
@@ -22,5 +23,25 @@ public class ProductService {
         return new ProductDto(product);
     }
 
+    @Transactional(readOnly = true)
+    public Page<ProductDto> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDto(x));
+    }
+    
+    
+    @Transactional
+    public ProductDto insert(@RequestBody ProductDto productDto) {
+
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        product.setImgUrl(productDto.getImgUrl());
+
+        product = repository.save(product);
+        return new ProductDto(product);
+
+    }
     
 }
