8521d59ec9158acb0f7b5966b4011bac262836ee
Luis Fernando <luis.almeida.faria@gmail.com>
2025-01-07 10:47:09 -0300

    Excessão de serviço

--- src/main/java/com/devsuperior/dscommerce/Services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/Services/ProductService.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dscommerce.Services;
+package com.devsuperior.dscommerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscommerce.dtos.ProductDto;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -18,7 +19,8 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDto findById(Long id) {
-        Product product = repository.findById(id).get();
+        Product product = repository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ProductDto(product);
     }
 
@@ -35,7 +37,6 @@ public class ProductService {
         copyDtoToEntity(productDto, entity);
         entity = repository.save(entity);
         return new ProductDto(entity);
-
     }
 
     @Transactional
@@ -44,7 +45,6 @@ public class ProductService {
         copyDtoToEntity(productDto, entity);
         entity = repository.save(entity);
         return new ProductDto(entity);
-
     }
 
     private void copyDtoToEntity(ProductDto dto, Product entity) {
@@ -52,6 +52,12 @@ public class ProductService {
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
+    }
+
+    
+    @Transactional
+    public void delete(Long id) {
+        repository.deleteById(id);
     }
     
 }
--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.devsuperior.dscommerce.Services.ProductService;
 import com.devsuperior.dscommerce.dtos.ProductDto;
+import com.devsuperior.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -48,5 +49,11 @@ public class ProductController {
     public ResponseEntity<ProductDto> update(@PathVariable Long id, @RequestBody ProductDto dto) {
         dto = service.update(id,dto);
         return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
